77c18eaeeae33d10a8759b7d66539b26f31e0213
Enderson Maia <endersonmaia@gmail.com>
2023-12-19 17:30:05 +0000

    feat: adapt to rollups-node:1.3.0
    This commit introduces a new validator.config section into values.yaml
    where you can override all the rollups-node configuration. There's also
    a script available to generate this section based on the Config.toml
    that exists on the rollups-node repository.

---
+++ charts/rollups-node/values-config-generator.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Script to convert values from rollups node TOML file to helm chart values YAML file with descriptions
+set -euo pipefail
+[[ -n ${DEBUG:-} ]] && set -x
+
+VALUES_FILE="values.yaml"
+
+# Process TOML file using yq and transform to YAML format
+echo "Processing Config.toml using yq and transforming it to YAML format..."
+
+cat "$VALUES_FILE".tpl > "$VALUES_FILE"
+
+ROLLUPS_NODE_VERSION="$(yq .global.image.tag "$VALUES_FILE")"
+
+# Convert TOML to YAML format and structure, keeping descriptions
+curl -fsSL https://raw.githubusercontent.com/cartesi/rollups-node/v${ROLLUPS_NODE_VERSION}/internal/config/generate/Config.toml \
+| yq -ptoml -oy '
+{"validator": { "config":  (
+.[] as $entry ireduce   ({};
+    . * $entry
+)
+| to_entries
+| with(.[];
+    .key headComment = "-- " + .value.description
+    | .value = .value.default // ""
+)
+| from_entries
+) }}
+' \
+| sed '/^validator:/d;/^$/d' >> "$VALUES_FILE"
+
+echo "Done."
